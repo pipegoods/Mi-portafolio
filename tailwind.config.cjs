761e4a5d71204b6feac9ed6f1b736cbc222b3dcd
pipegoods <pipe.jaider@gmail.com>
2022-08-13 20:35:14 -0500

    ✨ Main banner created

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['"Poppins"', 'sans-serif'],
+			serif: ['Georgia', 'serif'],
+			mono: ['Menlo', 'monospace'],
+		},
 	},
 	plugins: [],
 }
